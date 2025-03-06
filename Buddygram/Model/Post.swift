826648aa6445435af98e09ed677eb028bc281e9a
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-06 13:39:01 +0900

    Post, User 데이터 삭제

--- Buddygram/Model/Post.swift
+++ Buddygram/Model/Post.swift
@@ -4,33 +4,3 @@
 //
 //  Created by KimJunsoo on 3/6/25.
 //
-
-import Foundation
-import FirebaseFirestoreSwift
-
-struct Post: Identifiable, Codable {
-    @DocumentID var id: String?
-    let userId: String
-    let username: String
-    let userProfileImageUrl: String?
-    let caption: String
-    let imageUrl: String
-    var likes: Int
-    var commentCount: Int
-    let timestamp: Date
-    var didLike: Bool? = false
-    
-    // Firestore의 custom fields를 위한 CodingKeys
-    enum CodingKeys: String, CodingKey {
-        case id
-        case userId = "user_id"
-        case username
-        case userProfileImageUrl = "user_profile_image_url"
-        case caption
-        case imageUrl = "image_url"
-        case likes
-        case commentCount = "comment_count"
-        case timestamp
-        case didLike = "did_like"
-    }
-}
