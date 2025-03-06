2801e0ced6efd5322968418d99ee304bb97195a1
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-06 13:51:28 +0900

    Post, User 임시 데이터 및 변수명 작성

--- Buddygram/Model/User.swift
+++ Buddygram/Model/User.swift
@@ -4,3 +4,22 @@
 //
 //  Created by KimJunsoo on 3/6/25.
 //
+
+import Foundation
+import SwiftUI
+import Combine
+import Firebase
+
+struct User: Identifiable, Codable {
+    let id: String  // Firebase Auth UID
+    var username: String
+    var email: String
+    var fullName: String?
+    var profileImageURL: String?
+    var bio: String?
+    var joinDate: Date
+    var postsCount: Int
+    var followersCount: Int
+    var followingCount: Int
+    var likedPosts: [String]  // 좋아요한 게시물 ID 목록
+}
