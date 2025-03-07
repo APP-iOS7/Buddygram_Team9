303b36e86cc0892b0c691fd70081b8a6f159745d
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-07 09:25:44 +0900

    회원가입뷰 파일 이름 변경: SignUpView, SignUpViewModel / AuthViewModel 작업

--- Buddygram/ViewModel/Auth/AuthViewModel.swift
+++ Buddygram/ViewModel/Auth/AuthViewModel.swift
@@ -11,6 +11,40 @@ import Firebase
 import Combine
 
 class AuthViewModel: ObservableObject {
-    @Published var userSession: Firebase.User?
+    @Published var currentUser: User?
+    @Published var isAuthenticated = false
+    
+    // Firebase 연결전 로컬 시뮬레이션용 테스트 기본 사용자
+    private var users: [User] = [
+        User(id: UUID().uuidString,
+             username: "test",
+             email: "test@test.com",
+             profileImageURL: nil,
+             createdAt: Date())
+    ]
+    
+    // SignUp 회원가입
+    func signUp(username: String, email: String, password: String) -> Bool {
+        // 유효성 검사 : 이메일 중복 확인
+        if users.contains(where: { $0.email == email }) {
+            return false
+        }
+        
+        let newUser = User(
+            id: UUID().uuidString,
+            username: username,
+            email: email,
+            profileImageURL: nil,
+            createdAt: Date()
+        )
+        
+        // 사용자 배열에 추가 (실제로는 DB에 저장)
+        users.append(newUser)
+        
+        // 로그인 처리
+        currentUser = newUser
+        isAuthenticated = true
+        return true
+    }
     
 }
